be6b9d1173058558de25b27a9e8c744f8b6e4379
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-07 16:06:12 +0530

    signIn() and signOut() feature completed, and implemented in Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,6 @@
-import { auth } from "@/auth";
+import { auth, signOut, signIn } from "@/auth";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 
 const Navbar = async () => {
   const session = await auth();
@@ -12,7 +11,35 @@ const Navbar = async () => {
         <Link href="/">
           <Image src="/logo.png" alt="logo" width={144} height={30} />
         </Link>
-        <div className="flex items-center gap-5"></div>
+        <div className="flex items-center gap-5 text-black">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit">Logout</button>
+              </form>
+              <Link href={`/user/${session?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("github");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
       </nav>
     </header>
   );
