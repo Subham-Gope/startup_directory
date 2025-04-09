1e9d70adf244090877e1133dd930c2d58704229c
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-09 17:00:29 +0530

    (root)>page.tsx file updated with more info

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,12 @@ export default function Home() {
   return (
     <>
       <section className="pink_container">
-        <h1 className="heading">HOME</h1>
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect with Entrpreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
+        </p>
       </section>
     </>
   );
