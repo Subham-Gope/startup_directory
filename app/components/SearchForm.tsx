eb06164812ef77fb53f35b430d883afb47518e31
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-12 23:58:44 +0530

    changes made to SearchFormReset

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import Form from "next/form";
+import SearchFormReset from "./SearchFormReset";
 
 const SearchForm = () => {
+  const query = "test";
+
+ 
+
   return (
     <Form action="/" scroll={false} className="search-form">
       <input
         name="query"
-        defaultValue=""
+        defaultValue={query}
         className="search-input"
         placeholder="Search Startups"
       />
+
+      <div className="flex gap-2"
+      {query && <SearchFormReset/>}></div>
     </Form>
   );
 };
