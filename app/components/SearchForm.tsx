8848783fcdf2e3feb88ab55004a659851b453fdd
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-13 15:40:51 +0530

    the Form tag error, export default error and the query not getting recognized error solved

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -5,20 +5,19 @@ import SearchFormReset from "./SearchFormReset";
 const SearchForm = () => {
   const query = "test";
 
- 
-
   return (
-    <Form action="/" scroll={false} className="search-form">
-      <input
-        name="query"
-        defaultValue={query}
-        className="search-input"
-        placeholder="Search Startups"
-      />
+    <>
+      <Form action="/" scroll={false} className="search-form">
+        <input
+          name="query"
+          defaultValue={query}
+          className="search-input"
+          placeholder="Search Startups"
+        />
 
-      <div className="flex gap-2"
-      {query && <SearchFormReset/>}></div>
-    </Form>
+        <div className="flex gap-2">{query && <SearchFormReset />}</div>
+      </Form>
+    </>
   );
 };
 
