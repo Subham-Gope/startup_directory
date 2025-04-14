069dcfbf57c269b7b0824498ee991cbedbba4ad8
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-14 20:44:44 +0530

    files like Navbar.tsx, SearchForm.tsx,SearchFormReset.tsx moved to components outside app folder

--- components/ui/SearchForm.tsx
+++ components/ui/SearchForm.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Form from "next/form";
-import SearchFormReset from "./SearchFormReset";
+import SearchFormReset from "../../components/ui/SearchFormReset";
 import { Search } from "lucide-react";
 
 const SearchForm = ({ query }: { query?: string }) => {
