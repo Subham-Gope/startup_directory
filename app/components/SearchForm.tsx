7e39b855514b2226a06d5895ab70177666dee864
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-14 18:42:18 +0530

    search icon added from lucide react and import issue of the icon solved

--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Form from "next/form";
 import SearchFormReset from "./SearchFormReset";
+import { Search } from "lucide-react";
 
 const SearchForm = ({ query }: { query?: string }) => {
   return (
@@ -17,7 +18,7 @@ const SearchForm = ({ query }: { query?: string }) => {
           {query && <SearchFormReset />}
 
           <button type="submit" className="search-btn text-white">
-            s
+            <Search />
           </button>
         </div>
       </Form>
