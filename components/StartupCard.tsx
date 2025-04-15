37668d739f73597cdcc0a5959bcb6abb256b4b10
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-15 20:47:36 +0530

    Link tag added in StartupCard.tsx file

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -26,8 +26,12 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`}> </Link>
-          <p className="text-16-medium line-clamp-1">{name}</p>
+          <Link href={`/user/${authorId}`}>
+            <p className="text-16-medium line-clamp-1">{name}</p>
+          </Link>
+          <Link href={`/statup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1"></h3>
+          </Link>
         </div>
       </div>
     </li>
