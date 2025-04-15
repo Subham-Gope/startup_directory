c62fdc8615ae6e158bd6bb7c790dad19539f2a9b
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-15 20:27:21 +0530

    properties destructured from the post in StartupCard.tsx file

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,20 +4,30 @@ import Link from "next/link";
 import React from "react";
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author: { _id: authorId, name },
+    title,
+    category,
+    _id,
+    image,
+  } = post;
+
   return (
     <li className="startup-card group">
       <div className="flex-between">
-        <p className="startup_card_date">{formatDate(post._createdAt)}</p>
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
         <div className="flex gap-1.5">
           <EyeIcon className="size-6 text-primary" />
-          <span className="text-16-medium">{post.views}</span>
+          <span className="text-16-medium">{views}</span>
         </div>
       </div>
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${post.author?._id}`}> </Link>
-          <p className="text-16-medium line-clamp-1">{post.author?.name}</p>
+          <Link href={`/user/${authorId}`}> </Link>
+          <p className="text-16-medium line-clamp-1">{name}</p>
         </div>
       </div>
     </li>
