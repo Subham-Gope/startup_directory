d2d2fed730ac162f2d910a86b7e40f32b74dc726
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-15 14:55:37 +0530

    post._createdAt is rendered in StartupCard.tx file

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
-const StartupCard = () => {
-  return <div>StartupCard</div>;
+const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  return (
+    <li className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{post._createdAt}</p>
+      </div>
+    </li>
+  );
 };
 
 export default StartupCard;
