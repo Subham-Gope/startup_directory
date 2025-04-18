1426869a67324d0130993fd06b7ac36220ac6772
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-18 23:08:43 +0530

    more details added to the startup.ts page

--- sanity/schemaTypes/startup.ts
+++ sanity/schemaTypes/startup.ts
@@ -1,5 +1,33 @@
-import { defineType } from "sanity";
+import { defineField, defineType } from "sanity";
 
 export const author = defineType({
-    
-})
+  name: "startup",
+  title: "Startup",
+  type: "document",
+  fields: [
+    defineField({
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+      },
+    }),
+    defineField({
+      name: "author",
+      type: "reference",
+      to: { type: "author" },
+    }),
+    defineField({
+      name: "views",
+      type: "number",
+    }),
+    defineField({
+      name: "",
+      type: "",
+    }),
+  ],
+});
