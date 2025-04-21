cb1a53dfda5c468d14402880a797f42043ff8561
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-21 23:10:22 +0530

    h1 tag added to the startup/[id]/page.tsx file

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -3,7 +3,11 @@ import React from "react";
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  return <div>page</div>;
+  return (
+    <>
+      <h1 className="text-3xl">This is a startup number: {id}</h1>
+    </>
+  );
 };
 
 export default page;
