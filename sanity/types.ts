83388f2fbe8d67fcd11ff81a037c46d374fcabe4
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-23 20:52:32 +0530

    nextjs canary version installed for ppr, sanity/cli issue solved new types added in type.ts

--- sanity/types.ts
+++ sanity/types.ts
@@ -196,11 +196,32 @@ export type STARTUPS_QUERYResult = Array<{
   image: string | null;
   pitch: string | null;
 }>;
+// Variable: STARTUP_BY_ID_QUERY
+// Query: *[_type=="startup" && _id==$id][0] {      _id,       title,       slug,       _createdAt,       author-> {        _id,         name,         slug,        image,         bio      },       views,       description,       category,       image,       pitch    }
+export type STARTUP_BY_ID_QUERYResult = {
+  _id: string;
+  title: string | null;
+  slug: Slug | null;
+  _createdAt: string;
+  author: {
+    _id: string;
+    name: string | null;
+    slug: null;
+    image: string | null;
+    bio: string | null;
+  } | null;
+  views: number | null;
+  description: string | null;
+  category: string | null;
+  image: string | null;
+  pitch: string | null;
+} | null;
 
 // Query TypeMap
 import "@sanity/client";
 declare module "@sanity/client" {
   interface SanityQueries {
     "*[_type==\"startup\" && defined(slug.current) && (!defined($search) ||  title match $search  || category match $search || author->name match $search) ] | order(_createdAt desc){\n    _id, \n    title, \n    _type,\n    _updatedAt,\n    _rev,\n    slug, \n    _createdAt,\n    author -> {\n    _id, name, image, bio, _type,\n    _updatedAt,\n    _rev,\n    },\n    views, \n    description, \n    category, \n    image, \n    pitch\n    }": STARTUPS_QUERYResult;
+    "*[_type==\"startup\" && _id==$id][0] {\n      _id, \n      title, \n      slug, \n      _createdAt, \n      author-> {\n        _id, \n        name, \n        slug,\n        image, \n        bio\n      }, \n      views, \n      description, \n      category, \n      image, \n      pitch\n    }\n    ": STARTUP_BY_ID_QUERYResult;
   }
 }
