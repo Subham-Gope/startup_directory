cafae9727db2a257b2a71bb0208d49774ef1e295
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-24 18:07:27 +0530

    StartupCard.tsx file updated

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -31,7 +31,6 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </div>
 
       <div className="flex-between mt-5 gap-5">
-        ``
         <div className="flex-1">
           <Link href={`/user/${author?._id}`}>
             <p className="text-16-medium line-clamp-1">{author?.name}</p>
