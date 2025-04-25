3a9af9ee0948e1339218527d77988aa694f7f571
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-25 20:26:58 +0530

    Ping.tsx added to View.tsx

--- components/View.tsx
+++ components/View.tsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import Ping from "./Ping";
 
-const View = ({id}: {id:string}) => {
+const View = ({ id }: { id: string }) => {
   return (
     <div className="view-container">
-        <div className="absolute -top-2 -right-2"></div>
+      <div className="absolute -top-2 -right-2">
+        <Ping />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default View
+export default View;
