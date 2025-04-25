1fa4c89abe592040d9314dfaac95336470655c29
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-25 20:23:47 +0530

    two divs with className added

--- components/View.tsx
+++ components/View.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const View = ({id}: {id:string}) => {
   return (
-    <div>View</div>
+    <div className="view-container">
+        <div className="absolute -top-2 -right-2"></div>
+    </div>
   )
 }
 
