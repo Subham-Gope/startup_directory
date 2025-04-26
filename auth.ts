f01221fb2cf8e5d4c9f6df30497442b8419e3fce
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-26 14:19:01 +0530

    signIn function implemented in the auth.ts callback

--- auth.ts
+++ auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
 import { client } from "./sanity/lib/client";
 import { AUTHOR_BY_GITHUB_ID_QUERY } from "./sanity/lib/queries";
+import { writeClient } from "./sanity/lib/write-client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [GitHub],
@@ -10,6 +11,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       const existingUser = await client.fetch(AUTHOR_BY_GITHUB_ID_QUERY, {
         id: profile?.id,
       });
+
+      if (!existingUser) {
+        await writeClient.create({
+          _type: "author",
+          id: profile?.id,
+          name: user?.name,
+          username: profile?.login,
+          email: user?.email,
+          image: user?.image,
+          bio: profile?.bio || "",
+        });
+      }
+
+      return true
     },
   },
 });
