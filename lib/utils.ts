3e44c49777626194d62d50f49a84169c77c2abdb
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-26 13:04:06 +0530

    the formatVIews function in lib/utils.ts file has updated to account for the Thousand,  Million, and Billion Views

--- lib/utils.ts
+++ lib/utils.ts
@@ -17,5 +17,14 @@ export const formatViews = (views: number): string => {
   if (views == 1) {
     return `${views} View`;
   }
-  return `${views} Views`;
+  if (views === 0 || (views >= 1 && views < 1000)) {
+    return `${views} Views`;
+  }
+  if (views >= 1000 && views < 1000000) {
+    return `${Math.floor(views / 1000)}K Views`;
+  }
+  if (views >= 1000000 && views < 100000000) {
+    return `${Math.floor(views / 1000000)}M Views`;
+  }
+  return `${Math.floor(views / 1000000000)}B Views`;
 };
