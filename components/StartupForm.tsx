0555e10eb6185e052196b3867cb95ab98516d99f
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-27 14:48:04 +0530

    datalist added tot the Input tag for category

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 
 import { Input } from "./ui/input";
+import { Textarea } from "./ui/textarea";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -22,6 +23,65 @@ const StartupForm = () => {
         />
         {errors.title && <p className="startup-form_error">{errors.title}</p>}
       </div>
+      {/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+      <div>
+        <label htmlFor="description" className="startup-form_label">
+          Description
+        </label>
+        <Textarea
+          id="description"
+          name="description"
+          className="startup-form_textarea"
+          required
+          placeholder="Startup Description"
+        />
+        {errors.description && (
+          <p className="startup-form_error">{errors.description}</p>
+        )}
+      </div>
+
+      {/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+
+      <div>
+        <label htmlFor="category" className="startup-form_label">
+          Category
+        </label>
+        <Input
+          id="category"
+          name="category"
+          list="categoryList"
+          className="startup-form_input"
+          required
+          placeholder="Startup Category"
+        />
+        <datalist id="categoryList">
+          <option value="Health Tech" />
+          <option value="Fin Tech" />
+          <option value="Ed Tech" />
+          <option value="Green Tech" />
+        </datalist>
+        {errors.category && (
+          <p className="startup-form_error">{errors.category}</p>
+        )}
+      </div>
+
+      {/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
+
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
+        {errors.title && <p className="startup-form_error">{errors.title}</p>}
+      </div>
+
+      {/* \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\ */}
     </form>
   );
 };
