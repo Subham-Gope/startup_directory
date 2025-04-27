e6752a5a8d86cb57c7d267a1c9a0ae1da6e297e8
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-27 13:43:05 +0530

    heading of 'submit your startup added to the create/page.tsx'

--- app/(root)/startup/create/page.tsx
+++ app/(root)/startup/create/page.tsx
@@ -2,7 +2,7 @@ const page = () => {
   return (
     <>
       <section className="pink_container !min-h-[230px]">
-        <h1></h1>
+        <h1 className="heading">Submit Your Startup</h1>
       </section>
     </>
   );
