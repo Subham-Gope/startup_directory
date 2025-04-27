b018c4dfcafb701559756cdad3da686f9070f8fe
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-27 14:20:15 +0530

    input tag added to the StartupForm.tsx file

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Input } from "./ui/input";
+
 const StartupForm = () => {
   return (
     <form action={() => {}} className="startup-form">
@@ -7,6 +9,13 @@ const StartupForm = () => {
         <label htmlFor="title" className="startup-form_label">
           Title
         </label>
+        <Input
+          id="title"
+          name="title"
+          className="startup-form_input"
+          required
+          placeholder="Startup Title"
+        />
       </div>
     </form>
   );
