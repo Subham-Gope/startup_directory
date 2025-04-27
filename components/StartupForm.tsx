2fc8e3f6fd423218d6bb5b2d4f6798482192008b
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-27 14:17:47 +0530

    label tag added toe the StartupForm.tsx file

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,7 +1,15 @@
 "use client";
 
 const StartupForm = () => {
-  return <form action={() => {}} className="startup-form"></form>;
+  return (
+    <form action={() => {}} className="startup-form">
+      <div>
+        <label htmlFor="title" className="startup-form_label">
+          Title
+        </label>
+      </div>
+    </form>
+  );
 };
 
 export default StartupForm;
