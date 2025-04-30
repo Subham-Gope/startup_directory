ef4723242a81fc79b900bab98215e3f5a022d3ac
Subham Gope <subhamgopebusiness@gmail.com>
2025-04-30 12:07:49 +0530

    the validations.ts file updated

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,6 +1,28 @@
 import { z } from "zod";
 
 const formSchema = z.object({
-  title: z.string().min(3).max(100),
-  descripton: z.string().min(20).max(500),
+  title: z
+    .string()
+    .min(3, { message: "Must be 3 character long" })
+    .max(100, { message: "Must be less than 100 characters long" }),
+  descripton: z
+    .string()
+    .min(20, { message: "Must be 20 character long" })
+    .max(500, { message: "Must be less than 500 characters long" }),
+  category: z
+    .string()
+    .min(3, { message: "Must be 3 character long" })
+    .max(20, { message: "Must be less than 20 characters long" }),
+  link: z
+    .string()
+    .url({ message: "Invalid URL" })
+    .refine(async (url) => {
+      try {
+        const res = await fetch(url, { method: "HEAD" });
+        const contentType = res.headers.get("content-type");
+        return contentType?.startsWith("image/");
+      } catch {
+        return false;
+      }
+    }),
 });
