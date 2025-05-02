c1db8b67988a0e057c718ee72edd218aa7c48308
Subham Gope <subhamgopebusiness@gmail.com>
2025-05-02 15:10:44 +0530

    the startup registery code setup is done using the .create() method of the client object , here in this case it is write-client.create()

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import { auth } from "@/auth";
+import { parseServerActionResponse } from "./utils";
+import slugify from "slugify";
+import { writeClient } from "@/sanity/lib/write-client";
 
 export const createPitch = async (
   state: any,
@@ -21,5 +24,35 @@ export const createPitch = async (
 
   const slug = slugify(title as string, { lower: true, strict: true });
 
-  
+  try {
+    const startup = {
+      title,
+      description,
+      category,
+      image: link,
+      slug: {
+        _type: slug,
+        current: slug,
+      },
+      author: {
+        _type: "reference",
+        _ref: session?.id,
+      },
+      pitch,
+    };
+
+    const result = await writeClient.create({ _type: "startup", ...startup });
+
+    return parseServerActionResponse({
+      ...result,
+      error: "",
+      status: "SUCCESS",
+    });
+  } catch (error) {
+    console.error(error);
+    return parseServerActionResponse({
+      error: JSON.stringify(error),
+      status: "ERROR",
+    });
+  }
 };
