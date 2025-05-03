9029ac33a1e38d5de8636246430d564f61ba2816
Subham Gope <subhamgopebusiness@gmail.com>
2025-05-03 19:44:31 +0530

    user image configured in the user/[id]/page.tsx file

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/auth";
 import { client } from "@/sanity/lib/client";
 import { AUTHOR_BY_ID_QUERY } from "@/sanity/lib/queries";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 import React from "react";
 
@@ -15,11 +16,27 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
     return notFound();
   }
 
-  return <>
-  <section className="profile_container">
-    
-  </section>
-  </>;
+  return (
+    <>
+      <section className="profile_container">
+        <div className="profile_card">
+          <div className="profile_title">
+            <h3 className="text-24-black uppercase text-center line-clamp-1">
+              {user?.name}
+            </h3>
+          </div>
+
+          <Image
+            src={user.image}
+            alt={user.name}
+            width={220}
+            height={220}
+            className="profile_image"
+          />
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default page;
